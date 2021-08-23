c78b35233446ae98799ce64d820ad074b1f7fc84
lukeyuan31 <1393499408@qq.com>
2021-08-23 09:37:29 +0800

    update jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent none
+    stages {
+        stage('Build') {
+            agent {
+                docker {
+                    image 'maven:3.8.1-adoptopenjdk-11'
+                }
+            }
+        }
+        stage('Deploy to docker') {
+            agent any
+            steps {
+                sh 'docker-compose down'
+                sh 'docker build -f Dockerfile-mongodb -t MarketDataAnalysis/mongodb .'
+                sh 'docker build -f Dockerfile-app -t MarketDataAnalysis/app .'
+                sh 'docker-compose up -d'
+            }
+        }
+
+    }
+}
